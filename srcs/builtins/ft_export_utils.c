b37b108ec300ee4cbe41cec804a5ca2a22c14ac7
Christian Yerildt Arce Borro <carce-bo@student.42madrid.com>
2021-09-22 14:00:18 +0200

    he encontrado un free que no hacia falta, parcheado

--- srcs/builtins/ft_export_utils.c
+++ srcs/builtins/ft_export_utils.c
@@ -6,7 +6,7 @@
 /*   By: carce-bo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 11:48:14 by carce-bo          #+#    #+#             */
-/*   Updated: 2021/09/21 12:52:45 by carce-bo         ###   ########.fr       */
+/*   Updated: 2021/09/22 13:57:59 by carce-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,32 @@ void	create_new_environment(char *name, char *value)
 	g_shell->env = new_env;
 }
 
+/* There are two possibilities. If the value comes from the local
+ * environment, then the environment matrix has to add a new entry
+ * to it. If the value comes from the global env, then the
+ * correspondent entry is the one that has to change, thus the size
+ * of the matrix remains unchanged. */
+void	reconstruct_environment(char *name, char *value)
+{
+	int		entry;
+	char	*aux;
+
+	aux = NULL;
+	entry = find_env(name);
+	if (entry == -1)
+		create_new_environment(name, value);
+	else
+	{
+		aux = ft_strjoin(name, "=");
+		free(g_shell->env[entry]);
+		g_shell->env[entry] = ft_strjoin(aux, value);
+		free_three_ptrs(aux, name, value);
+	}
+}
+
 void	check_for_already_defined(char *name, char *value)
 {
 	int		prev_def;
-	int		entry;
 	char	*aux;
 
 	aux = value;
@@ -44,18 +66,9 @@ void	check_for_already_defined(char *name, char *value)
 	if (aux == value)
 		prev_def = KO;
 	else
-	{
 		prev_def = OK;
-		free(aux);
-	}
 	if (prev_def == OK)
-	{
-		entry = find_env(name);
-		aux = ft_strjoin(name, "=");
-		free(g_shell->env[entry]);
-		g_shell->env[entry] = ft_strjoin(aux, value);
-		free(aux);
-	}	
+		reconstruct_environment(name, value);
 	else
 		create_new_environment(name, value);
 }
